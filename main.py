6880e5946faf29daa6787c576e4221144aa4031b
Brincoveanu Tiberiu <brincoveanutiberiu@gmail.com>
2026-01-15 19:42:10 +0200

    feat: token metadata

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("Can't fine the API key!")
+    
+    client = genai.Client(api_key=api_key)
+    
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+    
+    if not response.usage_metadata:
+        raise RuntimeError("Failed API request!")
+    print(f"User prompt: {prompt}")    
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
