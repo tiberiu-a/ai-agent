9b538b7f64c4530539850a8acc5af5e499961593
Brincoveanu Tiberiu <brincoveanutiberiu@gmail.com>
2026-01-21 20:56:18 +0200

    restructured the code

--- main.py
+++ main.py
@@ -5,40 +5,44 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-
 def main():
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()    
+    prompt = args.user_prompt
+    isVerbose = args.verbose
+    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("Can't fine the API key!")
     
+    if isVerbose:
+        print(f"User prompt: {prompt}") 
+
     client = genai.Client(api_key=api_key)
     
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-    args = parser.parse_args()    
-    prompt = args.user_prompt
-    isVerbose = args.verbose
-
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
     
-    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+    generate_content(client, messages, isVerbose)
     
+def generate_content(client, messages, isVerbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages
     )
     
     if not response.usage_metadata:
-        raise RuntimeError("Failed API request!")
+        raise RuntimeError("Gemini API response appears to be malformed")
     
     if isVerbose:
-        print(f"User prompt: {prompt}")    
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         
     print(f"Response:\n{response.text}")
-
 
 if __name__ == "__main__":
     main()
